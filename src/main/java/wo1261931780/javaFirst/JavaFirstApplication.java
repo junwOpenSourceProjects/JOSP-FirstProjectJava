8df2d61f3cf87afdbaabb98565539ae8815fa006
junw <45444154+wo1261931780@users.noreply.github.com>
2023-03-15 16:34:49 +0800

    初始化

---
+++ src/main/java/wo1261931780/javaFirst/JavaFirstApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.javaFirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/javaFirst/JavaFirstApplicationTests.java
@@ -0,0 +1,16 @@
+package wo1261931780.javaFirst;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class JavaFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+		log.info("测试");
+	}
+
+}
